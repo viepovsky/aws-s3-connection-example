2373dcb932acd0dc8447c55c85b956d043e571e4
Oskar R <o.rajzner@gmail.com>
2023-11-13 10:14:50 +0100

    Add fetching data from backend to App.js

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -3,9 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+const UserProfiles = () => {
+  const fetchUserProfiles = () => {
+      axios.get("http://localhost:8080/api/v1/user-profile").then(response => {
+        console.log("Response from API:", response);
+      });
+  }
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+  return "Hello from UserProfiles";
+}
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <UserProfiles />
+    </div>
   );
 }
 
