723fca106f100ca3e2a726e4d2998149e503d0ab
Oskar R <o.rajzner@gmail.com>
2023-11-13 11:00:02 +0100

    Add drag n drop function to App.js

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -1,19 +1,56 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useDropzone} from 'react-dropzone'
+
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
   const fetchUserProfiles = () => {
       axios.get("http://localhost:8080/api/v1/user-profile").then(response => {
         console.log("Response from API:", response);
+        setUserProfiles(response.data);
       });
   }
 
   useEffect(() => {
     fetchUserProfiles();
   }, []);
-  return "Hello from UserProfiles";
+
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+
+        <br/>
+        <br/>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        <Dropzone />
+        <br/>
+      </div>
+    )
+  });
+}
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
 }
 
 function App() {
